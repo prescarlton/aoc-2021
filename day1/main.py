5dd982a7752484621386458d0d4014b420949838
Preston Carlton <prescarlton@gmail.com>
2021-12-01 14:26:15 -0500

    finished day 1 part 2

--- day1/main.py
+++ day1/main.py
@@ -1,14 +1,17 @@
-def main():
-    # first, read input file into a list of reported depths
+def get_depths():
+    # read input file into a list of reported depths
     with open("./input.txt", "r") as in_file:
         depths = [int(num) for num in in_file.readlines()]
+    return depths
 
+def main():
+    depths = get_depths()
     # store total num of depths larger than 1st record
-    total = 0
-    for i in range(len(depths)):
-        if depths[i] > depths[i - 1]:
-            total += 1
-    print(total)
+    total1 = len([i for i in range(len(depths)) if depths[i] > depths[i - 1]])
+    # store total num of sums greater than 1st 3-depth sum
+    total2 = len([i for i in range(len(depths)) if (sum(depths[i:i-3:-1])) > (sum(depths[i-1:i-4:-1]))])
+    print(f"Part 1 Answer: {total1}")
+    print(f"Part 1 Answer: {total2}")
 
 if __name__ == "__main__":
     main()
